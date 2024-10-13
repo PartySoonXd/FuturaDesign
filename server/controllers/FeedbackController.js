0cfa62e4726fa19f68a082993c3e8e9b683177d7
PartySoonXd <vladbelom2005@gmail.com>
2024-10-13 18:18:45 +0700

    create api controllers

---
+++ server/controllers/FeedbackController.js
@@ -0,0 +1,13 @@
+const FeedbackService = require('../services/FeedbackService')
+
+class FeedbackController {
+    async create(req, res) {
+
+    }
+
+    async get(req, res) {
+
+    }
+}
+
+module.exports = new FeedbackController()
---
+++ server/controllers/PortfolioController.js
@@ -0,0 +1,21 @@
+const PortfolioService = require('../services/PortfolioService')
+
+class PortfolioController {
+    async create(req, res) {
+
+    }
+
+    async delete(req, res) {
+
+    }
+
+    async get(req, res) {
+
+    }
+    
+    async getInCategory(req, res) {
+
+    }
+}
+
+module.exports = new PortfolioController()
