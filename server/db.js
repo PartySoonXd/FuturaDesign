6b3e4f8c8ec1e3cba199f497660d60f7fb45c6b7
PartySoonXd <vladbelom2005@gmail.com>
2024-10-14 23:37:39 +0700

    connect couch db

---
+++ server/db.js
@@ -0,0 +1,9 @@
+const user = process.env.COUCHDB_USER
+const password = process.env.COUCHDB_PASSWORD
+const host = process.env.COUCHDB_HOST
+
+const nano = require('nano')(`http://${user}:${password}@${host}:5984`);
+
+const db = nano.db.use(process.env.COUCHDB_NAME)
+
+module.exports = db
