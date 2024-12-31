aa2cc0600c38167e7e22f3853fa7c812dfed8281
PartySoonXd <vladbelom2005@gmail.com>
2025-01-01 05:32:33 +0700

    created warning component

---
+++ app/src/app/lib/PortfolioProjectWarning/PortfolioProjectWarning.jsx
@@ -0,0 +1,9 @@
+const PortfolioProjectWarning = () => {
+    return (
+        <div className="warning">
+            <h4 className="warning-text h4">This site is NOT real landing page for interior desing company. He was made as a portfolio project for representation of my skills.</h4>
+        </div>
+    )
+}
+
+export default PortfolioProjectWarning
